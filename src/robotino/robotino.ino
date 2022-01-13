3edd7f38766e78d0a6db0cc91ffe6b45abfaf5e2
Carl OS <carlos.g.prado@gmail.com>
2022-01-13 22:45:16 +0100

    Added class to sketch

--- src/robotino/robotino.ino
+++ src/robotino/robotino.ino
@@ -3,13 +3,18 @@
   *
   */
 
+#include <RangeFinder.h>
+
+
+// HC-SR04 ultrasonic module
+// trig pin = 7
+// echo pin = 8
+RangeFinder rf01(7, 8);
+
 
 void setup() {
     // Setup the serial connection
     Serial.begin(9600);
-
-    // HC-SR04 ultrasonic module
-    setup_rangefinder();
 }
 
 
@@ -17,5 +22,8 @@ void loop() {
     // Wait for serial data
     // while (!Serial) {}
     // if (Serial.available() > 0) { /* do stuff with serial data */ }
+
+    Serial.println(rf01.getDistance())
+    delay(500);
     }
 
